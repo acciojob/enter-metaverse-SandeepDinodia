577b5f21bb10e636be22014a170cf774894145a3
SandeepDinodia <151106767+SandeepDinodia@users.noreply.github.com>
2024-08-15 22:04:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+const para= document.getElementById("status");
+const btn= document.querySelector("button");
+
+btn.addEventListener("click",function(){
+    para.innerHTML="<h1>Entered Metaverse</h1>";
+})
